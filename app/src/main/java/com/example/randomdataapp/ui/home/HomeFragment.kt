c8ec036ff308fe5f1c3c55ebdfeadee6fcedbaed
Marcos Ramos <marcos.ramos@wansoft.com.br>
2023-08-15 19:58:08 -0300

    feat: Create main screen layout.

---
+++ app/src/main/java/com/example/randomdataapp/ui/home/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.randomdataapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.randomdataapp.R
+import com.example.randomdataapp.databinding.FragmentHomeBinding
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+    }
+}
