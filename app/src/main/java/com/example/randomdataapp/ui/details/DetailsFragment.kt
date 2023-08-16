5ddf3ca4a19de19a2dc38191646aabab283df9b6
Marcos Ramos <marcos.ramos@wansoft.com.br>
2023-08-16 14:45:54 -0300

    feat: Create viewHolder for each different display cells and build their respective adapters.

--- app/src/main/java/com/example/randomdataapp/ui/details/DetailsFragment.kt
+++ app/src/main/java/com/example/randomdataapp/ui/details/DetailsFragment.kt
@@ -1,21 +1,48 @@
 package com.example.randomdataapp.ui.details
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.randomdataapp.R
 import com.example.randomdataapp.databinding.FragmentDetailsBinding
+import com.example.randomdataapp.framework.imageLoader.ImageLoader
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @AndroidEntryPoint
 class DetailsFragment : Fragment(R.layout.fragment_details) {
 
     private lateinit var binding: FragmentDetailsBinding
+    private val args by navArgs<DetailsFragmentArgs>()
+
+    @Inject
+    lateinit var imageLoader: ImageLoader
+
+    private val detailsAdapter: DetailsAdapter by lazy {
+        val detailsAdapter = DetailsAdapter()
+        binding.recyclerDetails.adapter = detailsAdapter
+        return@lazy detailsAdapter
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentDetailsBinding.bind(view)
+        setupListener()
+        val userViewArgs = args.users
+
+        detailsAdapter.submitUsersData(
+            context = requireContext(),
+            users = userViewArgs,
+            creditCard = userViewArgs.creditCard,
+            subscription = userViewArgs.subscription
+        )
+    }
+
+    private fun setupListener() {
+        binding.buttonBack.setOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 }
