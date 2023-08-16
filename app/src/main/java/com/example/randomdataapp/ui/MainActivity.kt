f07bb980c5c967af5835806e1fe59c779221466b
Marcos Ramos <marcos.ramos@wansoft.com.br>
2023-08-16 09:57:54 -0300

    feat: Watch states in fragment and display list of users.

--- app/src/main/java/com/example/randomdataapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/randomdataapp/ui/MainActivity.kt
@@ -6,7 +6,9 @@ import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import com.example.randomdataapp.R
 import com.example.randomdataapp.databinding.ActivityMainBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
